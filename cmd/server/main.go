87b307d70948d7c5e4d2aabe1028b0b03ca384a0
Andrii Sozonik <aandrku@Andriis-MacBook-Air.local>
2025-01-05 13:09:54 -0800

    just testing

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type SomeStruct struct {
+	Name   string
+	Job    string
+	FooBar string
+}
 
 func main() {
 	fmt.Println("Hello, world")
 
+	mux := http.NewServeMux()
+
+	http.ListenAndServe(":8888", mux)
 }
